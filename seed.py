f17b3c93e2852f7b22eeae5c7ac9187c7aa18439
Pete Fitton <petefitton@gmail.com>
2025-10-02 16:56:54 -0600

    Clean up seed file

--- seed.py
+++ seed.py
@@ -13,28 +13,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 
-def get_record_from_attr(model, url_field, url):
+def get_record_from_url(model, url_field, url):
     return model.query.filter(getattr(model, url_field) == url).first()
-
-def get_id_from_url(model, url_field, url):
-    record = get_record_from_attr(model, url_field, url)
-    return record.id if record else None
-
-def get_ids_from_urls(model, url_field, urls):
-    ids = []
-    for url in urls:
-        record = get_id_from_url(model, url_field, url)
-        if record:
-            ids.append(record)
-    return ids
 
 def get_records_from_urls(model, urls):
     records = []
     for url in urls:
-        record = get_record_from_attr(model, 'url', url)
+        record = get_record_from_url(model, 'url', url)
         if record:
             records.append(record)
     return records
+
+def get_id_from_url(model, url_field, url):
+    record = get_record_from_url(model, url_field, url)
+    return record.id if record else None
 
 
 def import_films():
@@ -49,11 +41,6 @@ def import_films():
                     director=f['director'],
                     producer=f['producer'],
                     release_date=f['release_date'],
-                    # characters=get_ids_from_urls(Person, 'url', f.get('characters', [])),
-                    # planets=get_ids_from_urls(Planet, 'url', f.get('planets', [])),
-                    # starships=get_ids_from_urls(Starship, 'url', f.get('starships', [])),
-                    # vehicles=get_ids_from_urls(Vehicle, 'url', f.get('vehicles', [])),
-                    # species=get_ids_from_urls(Species, 'url', f.get('species', [])),
                     created=f['created'],
                     edited=f['edited'],
                     url=f['url'],
@@ -75,12 +62,6 @@ def import_people():
                     eye_color=p['eye_color'],
                     birth_year=p['birth_year'],
                     gender=p['gender'],
-                    # homeworld_id=get_id_from_url(Planet, 'url', p.get('homeworld', '')),
-                    # homeworld=get_record_from_attr(Planet, 'url', p.get('homeworld', '')),
-                    # films=get_ids_from_urls(Film, 'url', p.get('films', [])),
-                    # species=get_ids_from_urls(Species, 'url', p.get('species', [])),
-                    # starships=get_ids_from_urls(Starship, 'url', p.get('starships', [])),
-                    # vehicles=get_ids_from_urls(Vehicle, 'url', p.get('vehicles', [])),
                     created=p['created'],
                     edited=p['edited'],
                     url=p['url'],
@@ -103,8 +84,6 @@ def import_planets():
                     terrain=p['terrain'],
                     surface_water=p['surface_water'],
                     population=p['population'],
-                    # films=get_ids_from_urls(Film, 'url', p.get('films', [])),
-                    # residents=get_ids_from_urls(Person, 'url', p.get('residents', [])),
                     created=p['created'],
                     edited=p['edited'],
                     url=p['url'],
@@ -127,9 +106,6 @@ def import_species():
                     eye_colors=s['eye_colors'],
                     average_lifespan=s['average_lifespan'],
                     language=s['language'],
-                    # homeworld_id=get_id_from_url(Planet, 'url', s.get('homeworld', '')),
-                    # films=get_ids_from_urls(Film, 'url', s.get('films', [])),
-                    # people=get_ids_from_urls(Person, 'url', s.get('people', [])),
                     created=s['created'],
                     edited=s['edited'],
                     url=s['url'],
@@ -156,8 +132,6 @@ def import_starships():
                     hyperdrive_rating=s['hyperdrive_rating'],
                     MGLT=s['MGLT'],
                     starship_class=s['starship_class'],
-                    # films=get_ids_from_urls(Film, 'url', s.get('films', [])),
-                    # pilots=get_ids_from_urls(Person, 'url', s.get('pilots', [])),
                     created=s['created'],
                     edited=s['edited'],
                     url=s['url'],
@@ -182,8 +156,6 @@ def import_vehicles():
                     cargo_capacity=v['cargo_capacity'],
                     consumables=v['consumables'],
                     vehicle_class=v['vehicle_class'],
-                    # films=get_ids_from_urls(Film, 'url', v.get('films', [])),
-                    # pilots=get_ids_from_urls(Person, 'url', v.get('pilots', [])),
                     created=v['created'],
                     edited=v['edited'],
                     url=v['url'],
@@ -196,7 +168,7 @@ def import_film_relationships():
         with open('seeds/films.json') as file:
             films = json.load(file)
             for f in films:
-                film=get_record_from_attr(Film, 'url', f.get('url', ''))
+                film=get_record_from_url(Film, 'url', f.get('url', ''))
                 film.characters=get_records_from_urls(Person, f.get('characters', []))
                 film.planets=get_records_from_urls(Planet, f.get('planets', []))
                 film.starships=get_records_from_urls(Starship, f.get('starships', []))
@@ -209,9 +181,9 @@ def import_person_relationships():
         with open('seeds/people.json') as file:
             people = json.load(file)
             for p in people:
-                person=get_record_from_attr(Person, 'url', p.get('url', ''))
+                person=get_record_from_url(Person, 'url', p.get('url', ''))
                 person.homeworld_id=get_id_from_url(Planet, 'url', p.get('homeworld', ''))
-                person.homeworld=get_record_from_attr(Planet, 'url', p.get('homeworld', ''))
+                person.homeworld=get_record_from_url(Planet, 'url', p.get('homeworld', ''))
                 person.films=get_records_from_urls(Film, p.get('films', []))
                 person.species=get_records_from_urls(Species, p.get('species', []))
                 person.starships=get_records_from_urls(Starship, p.get('starships', []))
@@ -223,7 +195,7 @@ def import_planet_relationships():
         with open('seeds/planets.json') as file:
             planets = json.load(file)
             for p in planets:
-                planet=get_record_from_attr(Planet, 'url', p.get('url', ''))
+                planet=get_record_from_url(Planet, 'url', p.get('url', ''))
                 planet.films=get_records_from_urls(Film, p.get('films', []))
                 planet.residents=get_records_from_urls(Person, p.get('residents', []))
                 db.session.commit()
@@ -233,7 +205,7 @@ def import_species_relationships():
         with open('seeds/species.json') as file:
             species = json.load(file)
             for s in species:
-                spec=get_record_from_attr(Species, 'url', s.get('url', ''))
+                spec=get_record_from_url(Species, 'url', s.get('url', ''))
                 spec.homeworld_id=get_id_from_url(Planet, 'url', s.get('homeworld', ''))
                 spec.films=get_records_from_urls(Film, s.get('films', []))
                 spec.people=get_records_from_urls(Person, s.get('people', []))
@@ -244,7 +216,7 @@ def import_starship_relationships():
         with open('seeds/starships.json') as file:
             starships = json.load(file)
             for s in starships:
-                starship=get_record_from_attr(Starship, 'url', s.get('url', ''))
+                starship=get_record_from_url(Starship, 'url', s.get('url', ''))
                 starship.films=get_records_from_urls(Film, s.get('films', []))
                 starship.pilots=get_records_from_urls(Person, s.get('pilots', []))
                 db.session.commit()
@@ -254,7 +226,7 @@ def import_vehicle_relationships():
         with open('seeds/vehicles.json') as file:
             vehicles = json.load(file)
             for v in vehicles:
-                vehicle=get_record_from_attr(Vehicle, 'url', v.get('url', ''))
+                vehicle=get_record_from_url(Vehicle, 'url', v.get('url', ''))
                 vehicle.films=get_records_from_urls(Film, v.get('films', []))
                 vehicle.pilots=get_records_from_urls(Person, v.get('pilots', []))
                 db.session.commit()
